ae63a0141b9f265c444fc396ae4f196b01faa3a1
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-02-22 17:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let textInput = document.getElementById('text');
+let delayInput = document.getElementById('delay');
+let btn = document.getElementById('btn');
+let display = document.getElementById('output');
+
+btn.addEventListener("click",function(){
+	setTimeout(function(){
+		display.innerHTML = textInput.value;
+	}, delayInput.value);
+});
